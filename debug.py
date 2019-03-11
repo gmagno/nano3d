7605cbbe26f43fbe0f9580bddb3955caea18d961
Goncalo <goncalo.magno@gmail.com>
2019-03-11 00:46:43 -0300

    Make import after updating cwd

--- debug.py
+++ debug.py
@@ -2,11 +2,12 @@ import os
 import sys
 from pprint import pprint
 
-from nano3d.viewer import main
-
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 print('Working dir: {}'.format(os.getcwd()))
 print('sys.path:')
 pprint(sys.path)
+
+from nano3d.viewer import main
+
 main()
 
